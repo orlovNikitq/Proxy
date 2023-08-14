256c868e99d59c84fc89af4e07b185b5da0b95e1
orlovNikitq <130779328+orlovNikitq@users.noreply.github.com>
2023-08-14 11:14:28 +0300

    Add files via upload

---
+++ ConsoleApplication5/ConsoleApplication5/ConsoleApplication5.cpp
@@ -0,0 +1,35 @@
+﻿#include<iostream>
+#include<string>
+using namespace std;
+
+class Subject {
+public:
+	virtual void Request() = 0;
+};
+
+class RealSubject : public Subject {	
+public:
+	void Request() override{
+		cout << "RealSubject: обработка запроса." << endl;
+	} 
+};
+
+class ProxySubject : public Subject {
+	RealSubject* realSub;
+public:
+	ProxySubject() {
+		realSub = new RealSubject();
+	}
+
+	void Request() override {
+		cout << "ProxySubject: Подготовка перед передачей запроса."<<endl;
+		realSub->Request();
+		cout << "ProxySubject: Завершение после передачи запроса."<<endl;
+	}
+};
+
+int main() {
+	setlocale(LC_ALL, "rus");
+	ProxySubject proxy;
+	proxy.Request();
+}
